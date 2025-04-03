4fa9460780c5646857bf4395b74e72e6b48095c6
Francisco Ismael Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-03 18:14:21 +0200

    all

--- src/main/java/org/example/Warrior.java
+++ src/main/java/org/example/Warrior.java
@@ -2,14 +2,14 @@ package org.example;
 
 import java.util.Random;
 
-public class Warrior /*extends CharacterClass */ implements IAttacker {
+public class Warrior extends CharacterClass implements IAttacker {
 
     Random random = new Random();
     private int stamina = random.nextInt(41) + 10;
     private int strength=random.nextInt(10)+1;
 
     public Warrior( int stamina, int strength) {
-        //super(name,hp);
+        super(name,hp);
         this.stamina = stamina;
         this.strength = strength;
     }
