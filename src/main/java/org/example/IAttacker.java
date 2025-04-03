e9aeec0c2b8d6d1e84075c387ee1486857d0a45c
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-04-03 17:38:54 +0200

    Create class Warrior and create Inrterface IAttacker

--- src/main/java/org/example/IAttacker.java
+++ src/main/java/org/example/IAttacker.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public interface IAttacker {
+
+    public void attack();
+
+
 }
--- src/main/java/org/example/Warrior.java
+++ src/main/java/org/example/Warrior.java
@@ -1,4 +1,38 @@
 package org.example;
 
-public class Warrior {
+import java.util.Random;
+
+public class Warrior /*extends CharacterClass */ implements IAttacker {
+
+    Random random = new Random();
+    private int stamina = random.nextInt(41) + 10;
+    private int strength=random.nextInt(10)+1;
+
+    public Warrior( int stamina, int strength) {
+        //super(name,hp);
+        this.stamina = stamina;
+        this.strength = strength;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+
+    @Override
+    public void attack() {
+
+    }
 }
