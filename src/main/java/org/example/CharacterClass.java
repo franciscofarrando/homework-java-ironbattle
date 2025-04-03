d397e0cdba27cda2ae203f23d2a14e14e57ea004
Francisco Ismael Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-03 17:01:15 +0200

    const getters and setters

--- src/main/java/org/example/CharacterClass.java
+++ src/main/java/org/example/CharacterClass.java
@@ -1,4 +1,29 @@
 package org.example;
 
 public class CharacterClass {
+    private String id;
+    private String name;
+    private int hp;
+    private final boolean isAlive = true;
+
+    public CharacterClass(String name, int hp) {
+        setName(name);
+        setHp(hp);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
 }
